ff53116303a8b07098c35eefebffed25d7107636
Houcine <houcine.boukhari@outlook.com>
2022-05-15 16:55:01 +0100

    change caode

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 
 from flask import Flask,request,send_file
 from PIL import Image
-from segmentation import segment
+from segmentation import chanvese
 import io
 import base64
 import cv2
@@ -21,13 +21,19 @@ def process():
     img = Image.open(file)
     im_array = np.array(img)
     image = cv2.cvtColor(np.array(im_array),cv2.COLOR_BGR2GRAY)
-    result = segment(image)
-    res_n=cv2.normalize(result,dst=None,alpha=0,beta=255,norm_type=cv2.NORM_MINMAX,dtype=cv2.CV_8U)
-    out_image = Image.fromarray(res_n)
-    buff = io.BytesIO()
-    out_image.save(buff, format="PNG")
-    image_string = base64.b64encode(buff.getvalue()).decode("utf-8")
-    return image_string
+    mask = np.zeros(image.shape)
+    halfx = int(mask.shape[0] / 2)
+    halfy = int(mask.shape[1] / 2)
+    mask[halfx: halfx + 50, halfy: halfy + 50] = 255
+    chanvese(image,mask,max_its=1000,alpha=0.2,display=True)
+   # result = segment(image)
+    #res_n=cv2.normalize(result,dst=None,alpha=0,beta=255,norm_type=cv2.NORM_MINMAX,dtype=cv2.CV_8U)
+    #out_image = Image.fromarray(res_n)
+    #buff = io.BytesIO()
+    #out_image.save(buff, format="PNG")
+    #image_string = base64.b64encode(buff.getvalue()).decode("utf-8")
+    #return image_string
+    return "test"
 
 if __name__ == "__main__":
 
--- segmentation.py
+++ segmentation.py
@@ -1,85 +1,267 @@
+# ------------------------------------------------------------------------
+# Region Based Active Contour Segmentation
+#
+# seg = region_seg(I, init_mask, max_its, alpha, display)
+#
+# Inputs: I           2D image
+#         init_mask   Initialization (1 = foreground, 0 = bg)
+#         max_its     Number of iterations to run segmentation for
+#         alpha       (optional) Weight of smoothing term
+#                       higer = smoother.  default = 0.2
+#         display     (optional) displays intermediate outputs
+#                       default = true
+#
+# Outputs: seg        Final segmentation mask (1=fg, 0=bg)
+#
+# Description: This code implements the paper: "Active Contours Without
+# Edges" By Chan Vese. This is a nice way to segment images whose
+# foregrounds and backgrounds are statistically different and homogeneous.
+#
+# Example:
+# img = imread('tire.tif');
+# m = zeros(size(img));
+# m(33:33+117, 44:44+128) = 1;
+# seg = region_seg(img, m, 500);
+#
+# Coded by: Shawn Lankton (www.shawnlankton.com)
+# ------------------------------------------------------------------------
+#
+# Gif montage (save each step in a gif folder):
+# convert -delay 50 gif/levelset*.png levelset.gif
 
-
-from numpy import *
-import cv2
-import numpy
-from scipy.cluster.vq import *
-import matplotlib.pyplot as plt
 import numpy as np
+import scipy.ndimage as nd
+import cv2
 import matplotlib.pyplot as plt
-from scipy import ndimage as ndi
 
-from skimage import data
-from skimage.metrics import (adapted_rand_error,
-                             variation_of_information)
-from skimage.filters import sobel
-from skimage.measure import label
-from skimage.util import img_as_float
-from skimage.feature import canny
-from skimage.morphology import remove_small_objects
-from skimage.segmentation import (morphological_geodesic_active_contour,
-                                  inverse_gaussian_gradient,
-                                  watershed,
-                                  mark_boundaries)
-
-import matplotlib.pyplot as plt
-from skimage import data, img_as_float
-from skimage.segmentation import chan_vese
+eps = np.finfo(float).eps
 
 
+def chanvese(I, init_mask, max_its=200, alpha=0.2,
+             thresh=0, color='r', display=False):
+    I = I.astype(np.float)
 
-def segment(image):
-    # image = img_as_float(data.camera())
-    # Feel free to play around with the parameters to see how they impact the result
-    cv = chan_vese(image, mu=0.25, lambda1=1, lambda2=1, tol=1e-3, max_iter=200,
-                   dt=0.5, init_level_set="checkerboard", extended_output=True)
-    img_cv = cv[1]
+    # Create a signed distance map (SDF) from mask
+    phi = mask2phi(init_mask)
 
-    edges = sobel(img_cv)
-    im_test1 = watershed(edges, markers=468, compactness=0.001)
+    if display:
+        plt.ion()
+        fig, axes = plt.subplots(ncols=2)
+        show_curve_and_phi(fig, I, phi, color)
+        plt.savefig('levelset_start.png', bbox_inches='tight')
 
-    elevation_map = sobel(img_cv)
-    markers = np.zeros_like(img_cv)
-    markers[image < 30] = 1
-    markers[image > 150] = 2
-    im_true = watershed(elevation_map, markers)
-    im_true = ndi.label(ndi.binary_fill_holes(im_true - 1))[0]
+    # Main loop
+    its = 0
+    stop = False
+    prev_mask = init_mask
+    c = 0
 
-    edges = canny(img_cv)
-    fill_coins = ndi.binary_fill_holes(edges)
-    im_test2 = ndi.label(remove_small_objects(fill_coins, 21))[0]
+    while (its < max_its and not stop):
+        # Get the curve's narrow band
+        idx = np.flatnonzero(np.logical_and(phi <= 1.2, phi >= -1.2))
 
-    image = img_as_float(img_cv)
-    gradient = inverse_gaussian_gradient(image)
-    init_ls = np.zeros(image.shape, dtype=np.int8)
-    init_ls[10:-10, 10:-10] = 1
-    im_test3 = morphological_geodesic_active_contour(gradient, iterations=500,
-                                                     init_level_set=init_ls,
-                                                     smoothing=1, balloon=-1,
-                                                     threshold=0.69)
-    im_test3 = label(im_test3)
+        if len(idx) > 0:
+            # Intermediate output
+            if display:
+                if np.mod(its, 50) == 0:
+                    print('iteration: {0}'.format(its))
+                    show_curve_and_phi(fig, I, phi, color)
+            else:
+                if np.mod(its, 10) == 0:
+                    print('iteration: {0}'.format(its))
 
-    method_names = ['Compact watershed', 'Canny filter',
-                    'Morphological Geodesic Active Contours']
-    short_method_names = ['Compact WS', 'Canny', 'GAC']
+            # Find interior and exterior mean
+            upts = np.flatnonzero(phi <= 0)  # interior points
+            vpts = np.flatnonzero(phi > 0)  # exterior points
+            u = np.sum(I.flat[upts]) / (len(upts) + eps)  # interior mean
+            v = np.sum(I.flat[vpts]) / (len(vpts) + eps)  # exterior mean
 
-    precision_list = []
-    recall_list = []
-    split_list = []
-    merge_list = []
-    for name, im_test in zip(method_names, [im_test1, im_test2, im_test3]):
-        error, precision, recall = adapted_rand_error(im_true, im_test)
-        splits, merges = variation_of_information(im_true, im_test)
-        split_list.append(splits)
-        merge_list.append(merges)
-        precision_list.append(precision)
-        recall_list.append(recall)
-        print(f"\n## Method: {name}")
-        print(f"Adapted Rand error: {error}")
-        print(f"Adapted Rand precision: {precision}")
-        print(f"Adapted Rand recall: {recall}")
-        print(f"False Splits: {splits}")
-        print(f"False Merges: {merges}")
+            # Force from image information
+            F = (I.flat[idx] - u) ** 2 - (I.flat[idx] - v) ** 2
+            # Force from curvature penalty
+            curvature = get_curvature(phi, idx)
 
-    return mark_boundaries(image,im_true)
+            # Gradient descent to minimize energy
+            dphidt = F / np.max(np.abs(F)) + alpha * curvature
+
+            # Maintain the CFL condition
+            dt = 0.45 / (np.max(np.abs(dphidt)) + eps)
+
+            # Evolve the curve
+            phi.flat[idx] += dt * dphidt
+
+            # Keep SDF smooth
+            phi = sussman(phi, 0.5)
+
+            new_mask = phi <= 0
+            c = convergence(prev_mask, new_mask, thresh, c)
+
+            if c <= 5:
+                its = its + 1
+                prev_mask = new_mask
+            else:
+                stop = True
+
+        else:
+            break
+
+    # Final output
+    if display:
+        show_curve_and_phi(fig, I, phi, color)
+        plt.savefig('levelset_end.png', bbox_inches='tight')
+
+    # Make mask from SDF
+    seg = phi <= 0  # Get mask from levelset
+    return seg, phi, its
+
+
+# ---------------------------------------------------------------------
+# ---------------------- AUXILIARY FUNCTIONS --------------------------
+# ---------------------------------------------------------------------
+
+def bwdist(a):
+    """
+    Intermediary function. 'a' has only True/False vals,
+    so we convert them into 0/1 values - in reverse.
+    True is 0, False is 1, distance_transform_edt wants it that way.
+    """
+    return nd.distance_transform_edt(a == 0)
+
+
+# Displays the image with curve superimposed
+def show_curve_and_phi(fig, I, phi, color):
+    fig.axes[0].cla()
+    fig.axes[0].imshow(I, cmap='gray')
+    fig.axes[0].contour(phi, 0, colors=color)
+    fig.axes[0].set_axis_off()
+    plt.draw()
+
+    fig.axes[1].cla()
+    fig.axes[1].imshow(phi)
+    fig.axes[1].set_axis_off()
+    plt.draw()
+
+    plt.pause(0.001)
+
+
+def im2double(a):
+    a = a.astype(np.float)
+    a /= np.abs(a).max()
+    return a
+
+
+# Converts a mask to a SDF
+def mask2phi(init_a):
+    phi = bwdist(init_a) - bwdist(1 - init_a) + im2double(init_a) - 0.5
+    return phi
+
+
+# Compute curvature along SDF
+def get_curvature(phi, idx):
+    dimy, dimx = phi.shape
+    yx = np.array([np.unravel_index(i, phi.shape) for i in idx])  # subscripts
+    y = yx[:, 0]
+    x = yx[:, 1]
+
+    # Get subscripts of neighbors
+    ym1 = y - 1
+    xm1 = x - 1
+    yp1 = y + 1
+    xp1 = x + 1
+
+    # Bounds checking
+    ym1[ym1 < 0] = 0
+    xm1[xm1 < 0] = 0
+    yp1[yp1 >= dimy] = dimy - 1
+    xp1[xp1 >= dimx] = dimx - 1
+
+    # Get indexes for 8 neighbors
+    idup = np.ravel_multi_index((yp1, x), phi.shape)
+    iddn = np.ravel_multi_index((ym1, x), phi.shape)
+    idlt = np.ravel_multi_index((y, xm1), phi.shape)
+    idrt = np.ravel_multi_index((y, xp1), phi.shape)
+    idul = np.ravel_multi_index((yp1, xm1), phi.shape)
+    idur = np.ravel_multi_index((yp1, xp1), phi.shape)
+    iddl = np.ravel_multi_index((ym1, xm1), phi.shape)
+    iddr = np.ravel_multi_index((ym1, xp1), phi.shape)
+
+    # Get central derivatives of SDF at x,y
+    phi_x = -phi.flat[idlt] + phi.flat[idrt]
+    phi_y = -phi.flat[iddn] + phi.flat[idup]
+    phi_xx = phi.flat[idlt] - 2 * phi.flat[idx] + phi.flat[idrt]
+    phi_yy = phi.flat[iddn] - 2 * phi.flat[idx] + phi.flat[idup]
+    phi_xy = 0.25 * (- phi.flat[iddl] - phi.flat[idur] +
+                     phi.flat[iddr] + phi.flat[idul])
+    phi_x2 = phi_x ** 2
+    phi_y2 = phi_y ** 2
+
+    # Compute curvature (Kappa)
+    curvature = ((phi_x2 * phi_yy + phi_y2 * phi_xx - 2 * phi_x * phi_y * phi_xy) /
+                 (phi_x2 + phi_y2 + eps) ** 1.5) * (phi_x2 + phi_y2) ** 0.5
+
+    return curvature
+
+
+# Level set re-initialization by the sussman method
+def sussman(D, dt):
+    # forward/backward differences
+    a = D - np.roll(D, 1, axis=1)
+    b = np.roll(D, -1, axis=1) - D
+    c = D - np.roll(D, -1, axis=0)
+    d = np.roll(D, 1, axis=0) - D
+
+    a_p = np.clip(a, 0, np.inf)
+    a_n = np.clip(a, -np.inf, 0)
+    b_p = np.clip(b, 0, np.inf)
+    b_n = np.clip(b, -np.inf, 0)
+    c_p = np.clip(c, 0, np.inf)
+    c_n = np.clip(c, -np.inf, 0)
+    d_p = np.clip(d, 0, np.inf)
+    d_n = np.clip(d, -np.inf, 0)
+
+    a_p[a < 0] = 0
+    a_n[a > 0] = 0
+    b_p[b < 0] = 0
+    b_n[b > 0] = 0
+    c_p[c < 0] = 0
+    c_n[c > 0] = 0
+    d_p[d < 0] = 0
+    d_n[d > 0] = 0
+
+    dD = np.zeros_like(D)
+    D_neg_ind = np.flatnonzero(D < 0)
+    D_pos_ind = np.flatnonzero(D > 0)
+
+    dD.flat[D_pos_ind] = np.sqrt(
+        np.max(np.concatenate(
+            ([a_p.flat[D_pos_ind] ** 2], [b_n.flat[D_pos_ind] ** 2])), axis=0) +
+        np.max(np.concatenate(
+            ([c_p.flat[D_pos_ind] ** 2], [d_n.flat[D_pos_ind] ** 2])), axis=0)) - 1
+    dD.flat[D_neg_ind] = np.sqrt(
+        np.max(np.concatenate(
+            ([a_n.flat[D_neg_ind] ** 2], [b_p.flat[D_neg_ind] ** 2])), axis=0) +
+        np.max(np.concatenate(
+            ([c_n.flat[D_neg_ind] ** 2], [d_p.flat[D_neg_ind] ** 2])), axis=0)) - 1
+
+    D = D - dt * sussman_sign(D) * dD
+    return D
+
+
+def sussman_sign(D):
+    return D / np.sqrt(D ** 2 + 1)
+
+
+# Convergence Test
+def convergence(p_mask, n_mask, thresh, c):
+    p_mask = p_mask.astype(np.float)
+    n_mask = n_mask.astype(np.float)
+    diff = np.subtract(p_mask, n_mask)
+    n_diff = np.sum(np.abs(diff))
+    if n_diff < thresh:
+        c = c + 1
+    else:
+        c = 0
+    return c
+
+
 
